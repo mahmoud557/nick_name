3857f8caf3fcb9c95a4f2fd3a6371f6aaa6944ce
mahmoud557 <37010460+mahmoud557@users.noreply.github.com>
2022-03-26 17:29:51 -0700

    Delete managers directory

--- managers/t.js
+++
@@ -1,7 +0,0 @@
-var CryptoJS = require("crypto-js");
-var stringText={
-	parse:function(jsonStr){var j=JSON.parse(jsonStr);var cipherParams=CryptoJS.lib.CipherParams.create({ciphertext:CryptoJS.enc.Base64.parse(j.ct)});if(j.iv){cipherParams.iv=CryptoJS.enc.Hex.parse(j.iv)}if(j.s){cipherParams.salt=CryptoJS.enc.Hex.parse(j.s)}return cipherParams},
-	stringify:function(cipherParams){var j={ct:cipherParams.ciphertext.toString(CryptoJS.enc.Base64)};if(cipherParams.iv)j.iv=cipherParams.iv.toString();if(cipherParams.salt)j.s=cipherParams.salt.toString();return JSON.stringify(j)}
-}
-a=CryptoJS.AES.encrypt(JSON.stringify("with the best service, it provides its services 24 hours a day, 808306716"),"with the best service, it provid808306716",{format:stringText}).toString()
-console.log(a)
